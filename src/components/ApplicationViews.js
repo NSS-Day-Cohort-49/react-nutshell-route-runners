331ca234b91255a6723c8ff4693a1251a8f1594c
AlexLam12 <alex.e015@gmail.com>
2021-07-16 09:21:30 -0500

    friend module

--- src/components/ApplicationViews.js
+++ src/components/ApplicationViews.js
@@ -1,5 +1,8 @@
 import React from "react"
 import { Route } from "react-router-dom"
+import { FriendProvider } from "./friend/FriendProvider"
+import { FriendCard } from "./friend/FriendCard"
+import { FriendList } from "./friend/FriendList"
 
 export const ApplicationViews = () => {
   return (
@@ -8,9 +11,11 @@ export const ApplicationViews = () => {
       <Route exact path="/">
         {/* Render the component for news articles */}
       </Route>
-      <Route path="/friends">
-        {/* Render the component for list of friends */}
-      </Route>
+      <FriendProvider>
+        <Route path="/friends"><FriendList />
+          {/* Render the component for list of friends */}
+        </Route>
+      </FriendProvider>
       <Route path="/messages">
         {/* Render the component for the messages */}
       </Route>
