2a475b520a372ca334c35f75654133842ee5ed1d
lcurtis4 <lcurtis0104@gmail.com>
2021-07-19 14:12:59 -0500

    deleting tasks

--- src/components/task/TaskCard.js
+++ src/components/task/TaskCard.js
@@ -1,9 +1,18 @@
-import React from "react" 
+import React, { useContext } from "react" 
 import "./Task.css"
+import { TaskContext } from "./TaskProvider"
 
-export const TaskCard = ({ task }) => (
-    <section className="task">
-        <h3 className="task__name">{task.name}</h3>
-        <div className="task__date">{task.date}</div>
-    </section>
-)
+export const TaskCard = ({ task }) => {
+
+    const { deleteTask } = useContext(TaskContext)
+
+
+    return (
+        <section className="task">
+            <h3 className="task__name">{task.name}</h3>
+            <div className="task__date">{task.date}</div>
+            <button onClick={() => { deleteTask(task.id) }}>Delete Task</button>
+
+        </section>
+    )
+}
