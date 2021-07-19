4b9f85b6113e0e812e67a302b6f6a2df75d1b965
Thomas Witherow <twitherow@bellsouth.net>
2021-07-19 08:57:23 -0500

    pull request

--- src/events/EventCard.js
+++ src/events/EventCard.js
@@ -28,11 +28,11 @@ import React, { useContext }from "react"
       <article className="weatherAndButtons">
           <img className="weatherPic" src="https://media.gettyimages.com/photos/concept-photo-oxygen-with-white-cloud-picture-id1176356747?k=6&m=1176356747&s=612x612&w=0&h=65o22MsDpULjY-Y0X2X3ajpPSy2X5_ciTkzHZQBHjTA=" alt=""></img>
           <div className="eventButtons">
-          <button className="eventButton" onClick={() => {
+          {/* <button className="eventButton" onClick={() => {
               history.push(`/events/edit/${event.id}`)
-            }}>Edit</button>
-          <button className="eventButton" onClick={handleRelease}>Delete Event</button>
-          <button className="eventButton">Show Weather</button>
+            }}>Edit</button> */}
+          {/* <button className="eventButton" onClick={handleRelease}>Delete Event</button>
+          <button className="eventButton">Show Weather</button> */}
          </div>
       </article>
       </div>
