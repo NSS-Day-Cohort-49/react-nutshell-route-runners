d0d05c090fde9e199cea4774b578073156cc61ea
lcurtis4 <lcurtis0104@gmail.com>
2021-07-20 15:22:14 -0500

    merge issues

--- src/components/task/TaskCard.js
+++ src/components/task/TaskCard.js
@@ -12,7 +12,7 @@ export const TaskCard = ({ task }) => {
         updateTask({
             id: task.id, 
             name: task.name,
-            isComplete: false,
+            isComplete: true,
             userId: task.userId
         })
         .then(() => history.push("/tasks"))
